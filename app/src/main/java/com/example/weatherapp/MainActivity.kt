e8ac7d50722c78608981638bce5d46d7553b3994
pdcm11 <pdcm11@gmail.com>
2023-11-02 20:46:48 +0000

    Commit Final com MainPage e imagem do Sol

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -28,7 +31,9 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import com.example.weatherapp.ui.theme.WeatherAPPTheme
 import java.text.SimpleDateFormat
@@ -46,8 +51,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HeaderComponent()
-                    Info()
                 }
             }
         }
@@ -59,12 +62,13 @@ class MainActivity : ComponentActivity() {
 fun HeaderComponent(): Unit {
     Row(
         modifier = Modifier
+            .fillMaxWidth()
             .padding(12.dp, 8.dp),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically)
     {
         Button(onClick = { /*TODO*/ },
-            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent, contentColor = Color.White),
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
             modifier = Modifier
                 .size(40.dp),
             contentPadding = PaddingValues()
@@ -73,7 +77,7 @@ fun HeaderComponent(): Unit {
 
         {
             Image(
-                painter = painterResource(id = R.drawable.lupa),
+                painter = painterResource(id = R.drawable.search),
                 contentDescription = "Lupa",
             )
         }
@@ -87,7 +91,7 @@ fun HeaderComponent(): Unit {
         ) {
 
             Image(
-                painter = painterResource(id = R.drawable.skydive1),
+                painter = painterResource(id = R.drawable.foto),
                 contentDescription = "Foto")
 
         }
@@ -143,3 +147,60 @@ fun formatData(data: Date): String{
     return format.format(data)
 
 }
+
+@Preview(showBackground = true, widthDp = 380, heightDp = 800)
+@Composable
+fun MainPage(): Unit{
+    Column (
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth()
+    ){
+        HeaderComponent()
+        Info("Pedro")
+        ImagemCentral()
+    }
+}
+
+@OptIn(ExperimentalUnitApi::class)
+@Preview(showBackground = true, widthDp = 380, heightDp = 300)
+@Composable
+fun ImagemCentral(): Unit{
+    Box(modifier = Modifier
+        .height(300.dp)
+        .background(Color.Transparent)
+        .fillMaxWidth()
+    ){
+        Column {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(15.dp),
+                contentAlignment = Alignment.TopCenter
+            ){
+                Image(painter = painterResource(id = R.drawable.sun),
+                    contentDescription = "sol")
+            }
+
+            Row (verticalAlignment = Alignment.Bottom,
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ){
+                Text(text = "12ºC",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = TextUnit(55f, TextUnitType.Sp ),
+                    color = MaterialTheme.colorScheme.primary
+                )
+
+                Text(text = "Parcialmente nublado",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = TextUnit(15f, TextUnitType.Sp ),
+                    color = MaterialTheme.colorScheme.tertiary
+                )
+
+            }
+
+        }
+    }
+}
+
